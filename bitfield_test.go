95ce69c19108255586a72029f0ccf634398f580b
Yves Baumes <ybaumes@gmail.com>
2024-03-31 11:28:50 +0200

    Add more bitfield tests.

--- bitfield_test.go
+++ bitfield_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestGetBitfield(t *testing.T) {
 	var b byte = 0x0
@@ -24,7 +27,7 @@ func TestGetBitfield3(t *testing.T) {
 	var b byte = 0b10000000
 	for i := 0; i < 8; i++ {
 		if pres, err := BitField.Has(BitField{b}, i); err != nil {
-			t.Fatalf("Bitfield 0 is set, err = %v.", err)
+			t.Fatalf("Bitfield 0 is set, but we have an error, err = %v.", err)
 		} else {
 			if i == 0 && !pres {
 				t.Fatalf("First piece index is declared not present, while it should be. pres = %v.", pres)
@@ -32,5 +35,38 @@ func TestGetBitfield3(t *testing.T) {
 				t.Fatalf("Piece index different that the first one is declared as present, while it should not be. i=%v, pres = %v.", i, pres)
 			}
 		}
+	}
+}
+
+func TestGetBitfield4(t *testing.T) {
+	var b byte = 0b00000001
+	for i := 0; i < 8; i++ {
+		if pres, err := BitField.Has(BitField{b}, i); err != nil {
+			t.Fatalf("Bitfield 0 is set but we have an error, err = %v.", err)
+		} else {
+			if i == 7 && !pres {
+				t.Fatalf("Last piece index is declared not present, while it should be. pres = %v.", pres)
+			} else if i != 7 && pres {
+				t.Fatalf("Piece index different that the first one is declared as present, while it should not be. i=%v, pres = %v.", i, pres)
+			}
+		}
+	}
+}
+
+func TestSetBitfield1(t *testing.T) {
+	b := BitField{0b00000000}
+	BitField.Set(b, 0)
+	want := BitField{0b10000000}
+	if !reflect.DeepEqual(b, want) {
+		t.Fatalf("%b is the computed value, while expecting %v", b, want)
+	}
+}
+
+func TestSetBitfield2(t *testing.T) {
+	b := BitField{0b00000000}
+	BitField.Set(b, 7)
+	want := BitField{0b00000001}
+	if !reflect.DeepEqual(b, want) {
+		t.Fatalf("%b is the computed value, while expecting %v", b, want)
 	}
 }
